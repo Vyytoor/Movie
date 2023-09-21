19347deec0c2392d540121d4af43d5fde214776d
Vyytoor <givanildovitor104@gmail.com>
2023-09-20 21:21:05 -0300

    Subindo projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <h2> MetFlix &copy;2023 - Desenvolvido por Vitinho da quebrada</h2>
+        </footer>
+    );
+}
+
+export default Footer;
