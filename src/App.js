8c5c010433296b94c8f836b4ca1d0b13bb187dc4
Vyytoor <givanildovitor104@gmail.com>
2023-09-26 21:10:09 -0300

    Subindo Componente de Cards e links dos cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
+import videos from "./json/db.json"
+import Card from "./components/Card";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
       <Banner image="princ"/>
       <Container>
         <h1> Hello World</h1>
+        <section className="cards">
+          {videos.map((video)=> <Card id={video.id} key={video.id}/>)}
+        </section>      
       </Container>
       <Footer/>
     </>
